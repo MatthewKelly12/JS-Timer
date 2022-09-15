e2a4bbbcaf1493c5c3d981185b647f2c466bacc4
Matthew Kelly <mkonabike@gmail.com>
2022-09-15 10:26:26 -0500

    added js code for timer and display

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log('dis test');
+let countdown;
+const timerDisplay = document.querySelector("#timer-display");
+
+function timer(seconds)
+{
+	const now = Date.now();
+	const then = now + seconds * 1000;
+	displayTimeLeft(seconds);
+
+	countdown = setInterval(()=>
+		{
+			const secondsLeft = Math.round((then - Date.now())/1000);
+
+			if (secondsLeft < 0)
+			{
+				clearInterval(countdown);
+				return;
+			}
+			displayTimeLeft(secondsLeft);
+		},1000
+	)
+}
+
+function displayTimeLeft(seconds)
+{
+	const minutes = Math.floor(seconds/60);
+	const remainderSeconds = seconds % 60;
+	const display = `${minutes}:${remainderSeconds < 10 ? '0':''}${remainderSeconds}`;
+	timerDisplay.textContent = display;
+}
+
+timer(80);
